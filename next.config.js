5ddd388745903de4573a8d3b69c02a7c6c0346fa
Darragh <daradona10@gmail.com>
2021-09-09 13:11:50 +0100

    adding map functionality

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZGFyYWRvbmExMCIsImEiOiJja3Rjc3l2OHowZzJqMm9tYWtpY3E1dGt0In0.ttiivSfoWK3_rJqHkriqpA",
+  },
 };
